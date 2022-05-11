8e59fcd7fbb7047795845e383b6c7e4785d2f572
halcombian <103285162+halcombian@users.noreply.github.com>
2022-05-10 21:10:40 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
-document.createElement("div").setAttribute("id", "grid-container");
 const gridContainer = document.getElementById("grid-container");
+const newGridButton = document.getElementById("new-grid-button");
+
+function getGridWidth() {
+	return prompt("Please enter grid width", "16");
+}
+
+function createGrid() {
+	const gridWidth = getGridWidth();
+	gridContainer.setAttribute(
+		"style",
+		`grid-template-columns: repeat(${gridWidth}, 1fr);`
+	);
+	let gridSize = Math.pow(gridWidth, 2);
+	for (let i = 0; i < gridSize; i++) {
+		const gridElement = document.createElement("div");
+		gridElement.className = "grid-element";
+		gridContainer.append(gridElement);
+		gridElement.addEventListener("mouseenter", () => {
+			gridElement.style.backgroundColor = "black";
+		});
+	}
+}
+
+function newGrid() {
+	newGridButton.addEventListener("click", () => {
+		while (gridContainer.firstChild) {
+			gridContainer.removeChild(gridContainer.lastChild);
+		}
+		createGrid();
+	});
+}
+
+newGrid();
